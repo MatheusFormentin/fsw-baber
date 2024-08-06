a4d148fa6298f4d44735df00e89f9a0c4d4c9e2d
Matheus Formentin <formentinmatheus1@gmail.com>
2024-08-06 19:15:24 -0300

    feat: add header and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/Header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-export default function Home() {
+const Home = () => {
   return (
-    <Button>Teste</Button>
-  );
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Matheus! </h2>
+        <p>Terça Feira, 06 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua Busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende com os melhores FSW Barber"
+            className="rounded-xl object-cover"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Home
