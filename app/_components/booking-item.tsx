86a32b8aae60f39f9430fbfeb2d8371bbbde9e5c
Matheus Formentin <formentinmatheus1@gmail.com>
2024-08-08 21:59:11 -0300

    feat: ensure barbershops can be serached by title and services

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,7 +1,8 @@
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
-import { Card, CardContent } from "./ui/card"
+import { Avatar, AvatarImage } from "./ui/avatar"
 import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
 
+// TODO: receber agendamento como prop
 const BookingItem = () => {
   return (
     <>
@@ -10,9 +11,11 @@ const BookingItem = () => {
       </h2>
       <Card>
         <CardContent className="flex justify-between p-0">
+          {/* ESQUERDA */}
           <div className="flex flex-col gap-2 py-5 pl-5">
             <Badge className="w-fit">Confirmado</Badge>
             <h3 className="font-semibold">Corte de Cabelo</h3>
+
             <div className="flex items-center gap-2">
               <Avatar className="h-6 w-6">
                 <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
@@ -20,6 +23,7 @@ const BookingItem = () => {
               <p className="text-sm">Barbearia FSW</p>
             </div>
           </div>
+          {/* DIREITA */}
           <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
             <p className="text-sm">Agosto</p>
             <p className="text-2xl">05</p>
--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -17,115 +17,112 @@ import {
 import { signIn, signOut, useSession } from "next-auth/react"
 import { Avatar, AvatarImage } from "./ui/avatar"
 
-const SideBarSheet = () => {
+const SidebarSheet = () => {
   const { data } = useSession()
   const handleLoginWithGoogleClick = () => signIn("google")
   const handleLogoutClick = () => signOut()
 
-  return (
-    <div>
-      <SheetContent className="overflow-y-auto">
-        <SheetHeader>
-          <SheetTitle className="text-left">Menu</SheetTitle>
-        </SheetHeader>
+  console.log(data?.user)
 
-        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-          {data?.user ? (
-            <div className="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage
-                  src={data?.user?.image ?? ""}
+  return (
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+
+            <div>
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <DialogHeader>
+                  <DialogTitle>Faça login na plataforma</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta do Google.
+                  </DialogDescription>
+                </DialogHeader>
+
+                <Button
+                  variant="outline"
+                  className="gap-1 font-bold"
+                  onClick={handleLoginWithGoogleClick}
+                >
+                  <Image
+                    alt="Fazer login com o Google"
+                    src="/google.svg"
+                    width={18}
+                    height={18}
+                  />
+                  Google
+                </Button>
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamentos
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <SheetClose key={option.title} asChild>
+            <Button className="justify-start gap-2" variant="ghost" asChild>
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
                   height={18}
                   width={18}
                 />
-              </Avatar>
-
-              <div>
-                <p className="font-bold">{data.user.name}</p>
-                <p className="text-xs">{data.user.email}</p>
-              </div>
-            </div>
-          ) : (
-            <>
-              <h2 className="font-bold">Olá faça seu login!</h2>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button size="icon">
-                    <LogInIcon />
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="w-[90%]">
-                  <DialogHeader>
-                    <DialogTitle>Faça seu login na plataforma</DialogTitle>
-                    <DialogDescription>
-                      Conecte-se usando a sua conta do Google.
-                    </DialogDescription>
-                  </DialogHeader>
-                  <Button
-                    variant="outline"
-                    className="gap-1 font-bold"
-                    onClick={handleLoginWithGoogleClick}
-                  >
-                    <Image
-                      src="/google.svg"
-                      width={18}
-                      height={18}
-                      alt="Imagem do Google"
-                    />
-                    Google
-                  </Button>
-                </DialogContent>
-              </Dialog>
-            </>
-          )}
-        </div>
-
-        <div className="flex flex-col gap-2 border-b border-solid py-5">
-          <SheetClose asChild>
-            <Button className="justify-start gap-2" variant="ghost" asChild>
-              <Link href="/">
-                <HomeIcon size={18} />
-                Início
+                {option.title}
               </Link>
             </Button>
           </SheetClose>
-          <Button className="justify-start gap-2" variant="ghost">
-            <CalendarIcon size={18} />
-            Agendamentos
-          </Button>
-        </div>
+        ))}
+      </div>
 
-        <div className="flex flex-col gap-2 border-b border-solid py-5">
-          {quickSearchOptions.map((option) => (
-            <Button
-              key={option.title}
-              className="justify-start gap-2"
-              variant="ghost"
-            >
-              <Image
-                src={option.imageUrl}
-                alt={option.title}
-                width={18}
-                height={18}
-              />
-              {option.title}
-            </Button>
-          ))}
-        </div>
-
-        <div className="flex flex-col gap-2 py-5">
-          <Button
-            variant="ghost"
-            className="justify-start gap-2"
-            onClick={handleLogoutClick}
-          >
-            <LogOutIcon size={18} />
-            Sair da Conta
-          </Button>
-        </div>
-      </SheetContent>
-    </div>
+      <div className="flex flex-col gap-2 py-5">
+        <Button
+          variant="ghost"
+          className="justify-start gap-2"
+          onClick={handleLogoutClick}
+        >
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
   )
 }
 
-export default SideBarSheet
+export default SidebarSheet
